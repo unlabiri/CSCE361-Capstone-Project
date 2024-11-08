ba38305ca90013d717cdaacc891e0597eda9e918
zramazing <zramazing@gmail.com>
2024-11-07 19:30:07 -0600

    added comments to describe methods

--- VotingSystem/DatabaseInfo.cs
+++ VotingSystem/DatabaseInfo.cs
@@ -4,24 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Author: Zephyr Rose
+//Created 11/7/24
+
 namespace VotingSystem
 {
     internal class GetDatabaseInfo
     {
+
+        //allows a new user to be added, will eventually take many arguments
         public static bool AddNewUser()
         {
             return False;
     }
 
+        //allows a user to be deleted, will take the userID as argument
         public static bool DeleteUser()
         {
             return false;
         }
 
+        //returns the name of a user from the database, will take userID as an argument
+
         public static string GetName()
         {
             return Null;
         }
+
+        //returns the user's voter ID from the database, will take userID as an argument
 
         public static int GetVoterId()
         {

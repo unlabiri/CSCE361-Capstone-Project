deff06b4c9c488c62d89f9d2388da40a5e452c16
zramazing <zramazing@gmail.com>
2024-11-07 19:21:21 -0600

    added a skeleton class for getting info from SQL server

---
+++ VotingSystem/DatabaseInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VotingSystem
+{
+    internal class GetDatabaseInfo
+    {
+        public static bool AddNewUser()
+        {
+            return False;
+    }
+
+        public static bool DeleteUser()
+        {
+            return false;
+        }
+
+        public static string GetName()
+        {
+            return Null;
+        }
+
+        public static int GetVoterId()
+        {
+            return Null; 
+        }
+    }
+
+
+}
+

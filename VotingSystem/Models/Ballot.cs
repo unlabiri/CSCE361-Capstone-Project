03ddcc3ddc5b109a86cca52f750cd317abb1a81f
unlabiri <111925801+unlabiri@users.noreply.github.com>
2024-11-11 01:28:48 -0600

    intialized ballot object, (empty)

--- VotingSystem/Models/Ballot.cs
+++ VotingSystem/Models/Ballot.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//ballot for empty, unfilled ballot: does not collect vote data
+//author: Ada Aljabiri dated 11/11/2024
 namespace VotingSystem.Models
 {
     internal class Ballot
     {
+        public int ElectionID { get; set; }
+        public List<Candidate> Candidates { get; set; }
+
+        public Ballot(int electionID, List<Candidate> candidates) {
+            ElectionID = electionID;
+            Candidates = candidates;
+        }
      
     }
 }

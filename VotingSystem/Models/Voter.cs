4cf1cce0cbc7fd241f3ee7df6267b3b163d479d4
unlabiri <111925801+unlabiri@users.noreply.github.com>
2024-11-11 01:24:22 -0600

    initialized voter object

--- VotingSystem/Models/Voter.cs
+++ VotingSystem/Models/Voter.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// author: Ada Aljabiri dated 11/10/2024
 
 namespace VotingSystem.Models
 {
     internal class Voter
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public int UserId { get; set; }
+        public string Password { get; set; }
+        public string ZipCode { get; set; }
+
+        //initializes a voter Object
+        public Voter(int userId, string firstName, string lastName, DateTime birthday, DateTime registrationDate, string phoneNumber, string password, string zipCode) {
+            UserId = userId;
+            FirstName = firstName;
+            LastName = lastName;
+            Birthday = birthday;
+            RegistrationDate = registrationDate;
+            PhoneNumber = phoneNumber;
+            Password = password;
+            ZipCode = zipCode;
+        }
     }
 }

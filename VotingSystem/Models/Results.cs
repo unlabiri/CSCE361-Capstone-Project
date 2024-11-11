13da01d772b8287ab7efb02d225c743a90b83543
unlabiri <111925801+unlabiri@users.noreply.github.com>
2024-11-11 02:26:16 -0600

    fixed results comments

--- VotingSystem/Models/Results.cs
+++ VotingSystem/Models/Results.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-name: Ada Aljabiri dated: 11/11/2024
+// name: Ada Aljabiri dated: 11/11/2024
+ // Not a model class   
 namespace VotingSystem.Models
 {
     internal class Results

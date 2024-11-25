9ee907602099f9f9318967deebe6d484b8ce90cf
zramazing <zramazing@gmail.com>
2024-11-25 12:04:26 -0600

    updated databaseInfo and voter classes to remove registration date, add voter verification number

--- VotingSystem/DatabaseInfo.cs
+++ VotingSystem/DatabaseInfo.cs
@@ -13,29 +13,49 @@ namespace VotingSystem
     {
 
         //allows a new user to be added, will eventually take many arguments
-        public static bool AddNewUser(int userId, string firstName, string lastName, DateTime birthday, DateTime registrationDate, string phoneNumber, string username, string password, string zipCode)
+        public static bool AddNewUser(int userId, string firstName, string lastName, DateTime birthday, string phoneNumber, string username, string password, string zipCode, string voterVerificationNumber)
         {
             return false;
-    }
+        }
 
         //allows a user to be deleted, will take the userID as argument
-        public static bool DeleteUser(int userId, string firstName, string username)
+        public static bool DeleteUser(int userId, string username)
         {
             return false;
         }
 
         //returns the name of a user from the database, will take userID as an argument
 
-        public static string GetName(int userId)
+        public static string GetName(int userId, string userName)
         {
             return null;
         }
 
         //returns the user's voter ID from the database, will take userID as an argument
 
-        public static int GetVoterId(int userId)
+        public static int GetVoterId(int userId, string userName)
         {
             return 0; 
+        }
+
+        //returns the voters status for a specific vote
+        public static bool GetVoteStatus(int userId, string voterVerificationNumber, int ElectionId)
+        {
+            return false; 
+        }
+
+        //returns all current elections electionIDs, so that the info for each election
+        //can be taken from this list of IDs
+        public static List<int> GetAllElections()
+        {
+            return null;
+        }
+
+        //returns info for one election based on an election ID
+        //TODO: change return type 
+        public static bool GetElectionInfo(int ElectionId)
+        {
+            return false;
         }
     }
 
--- VotingSystem/Models/Voter.cs
+++ VotingSystem/Models/Voter.cs
@@ -13,24 +13,24 @@ namespace VotingSystem.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime Birthday { get; set; }
-        public DateTime RegistrationDate { get; set; }
         public string PhoneNumber { get; set; }
         public int UserId { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string ZipCode { get; set; }
+        public string VoterVerificationNumber { get; set; }
 
         //initializes a voter Object
-        public Voter(int userId, string firstName, string lastName, DateTime birthday, DateTime registrationDate, string phoneNumber, string username, string password, string zipCode) {
+        public Voter(int userId, string firstName, string lastName, DateTime birthday, string phoneNumber, string username, string password, string zipCode, string voterVerificationNumber) {
             UserId = userId;
             FirstName = firstName;
             LastName = lastName;
             Birthday = birthday;
-            RegistrationDate = registrationDate;
             PhoneNumber = phoneNumber;
             Username = username;
             Password = password;
             ZipCode = zipCode;
+            VoterVerificationNumber = voterVerificationNumber;
         }
     }
 }

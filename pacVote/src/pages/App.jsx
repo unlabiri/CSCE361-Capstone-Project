02edaf5dbf2ddade318d05b5abe6ba8d267a7109
Sean Casey <caseysean55@gmail.com>
2024-12-02 11:00:22 -0600

    All UI pages except for Ballot are mostly complete.

--- pacVote/src/pages/App.jsx
+++ pacVote/src/pages/App.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login"; 
 import CreateAccount from "./pages/CreateAccount"; 
+import Home from "./pages/Home";
+import Ballot from "./pages/Ballot";
+import Confirm from "./pages/Confirm";
+import Results from "./pages/Results";
+import Review from "./pages/Review";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" index element={<Login />} />
         <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/ballot" element={<Ballot />} />
+        <Route path="/confirm" element={<Confirm />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/review" element={<Review />} />
       </Routes>
     </Router>
   );
--- pacVote/src/pages/main.jsx
+++ pacVote/src/pages/main.jsx
@@ -4,7 +4,9 @@ import '../styles/index.css'
 import Login from './Login.jsx'
 import CreateAccount from './CreateAccount.jsx'
 import Home from './Home.jsx'
-
+import Results from './Results.jsx'
+import Confirm from './Confirm.jsx'
+import Review from './Review.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

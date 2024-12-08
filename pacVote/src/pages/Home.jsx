2f1d2b46166a1c9a4016ced12f18dcedc7a8d59e
rcamelin2 <rcamelin2@huskers.unl.edu>
2024-12-08 17:10:12 -0600

    Updating Home.jsx so redirect works correctly!

--- pacVote/src/pages/Home.jsx
+++ pacVote/src/pages/Home.jsx
@@ -1,25 +1,40 @@
 import React from "react";
 import logo from '../assets/logo.png';
 import '../styles/Home.css';
+import { useNavigate } from "react-router-dom";
 
 function clickMe() {
     alert('PacVote is an online voting system app for citizens of Pacopolis!');
 }
 
 function Home() {
+    const navigate = useNavigate();
+    
+    const goToLogin = () => {
+        navigate("/login");
+      };
+
+    const goToBallot = () => {
+        navigate("/ballot");
+      };
+
+    const goToResults = () => {
+        navigate("/results");
+      };
+
     return (
         <div className="Home-container">
             <div className="header-container">
                 <img src={logo} className="Home-logo" alt="PacVote Logo" />
                 <h1 className="Home-title">Welcome to PacVote - Voting System!</h1>
-                <button className="logout-button">Logout</button>
+                <button onClick={goToLogin} className="logout-button">Logout</button>
             </div>
 
             <div className="Home-box">
                 <h2 className="box-title">Current Votes:</h2>
                 <div className="vote-row">
                     <span>• Candidate1 vs Candidate2, voting ends Date</span>
-                    <button className="vote-button">Vote</button>
+                    <button onClick={goToBallot} className="vote-button">Vote</button>
                 </div>
             </div>
 
@@ -27,7 +42,7 @@ function Home() {
                 <h2 className="box-title">Past Votes:</h2>
                 <div className="results-row">
                     <span>• Winner Candidate3! Ended Date, % of the votes</span>
-                    <button className="results-button">Results</button>
+                    <button onClick={goToResults} className="results-button">Results</button>
                 </div>
             </div>
 

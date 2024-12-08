40bd549051d556a2b78c6c7f777e5ef1744b3cdd
rcamelin2 <rcamelin2@huskers.unl.edu>
2024-12-08 17:11:54 -0600

    Updating Results.jsx so redirect works correctly

--- pacVote/src/pages/Results.jsx
+++ pacVote/src/pages/Results.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import logo from '../assets/logo.png';
 import '../styles/Results.css';
+import { useNavigate } from "react-router-dom";
 
 function clickMe() {
     alert('PacVote is an online voting system app for citizens of Pacopolis!');
 }
 
 function Results() {
+    const navigate = useNavigate();
+    
+    const goToHome = () => {
+      navigate("/home");
+    };
+
     return (
         <div className="results-container">
             <div className="header-container">
                 <img src={logo} className="results-logo" alt="PacVote Logo" />
                 <h1 className="results-title">PacVote - Election Results - Date</h1>
-                <button className="home-button">Home Page</button>
+                <button onClick={goToHome} className="home-button">Home Page</button>
                 <div className="divider"></div>
-                <button className="logout-button">Logout</button>
             </div>
 
             <div className="results-box">
